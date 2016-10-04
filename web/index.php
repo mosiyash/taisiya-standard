0ac00da871e73b0eaf2d64edf41e59165e55a04e
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-10-04 12:33:11 +0700

    added pimple.json generator

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use JBZoo\PimpleDumper\PimpleDumper;
 use Slim\App;
 use Slim\Container;
 use Taisiya\CoreBundle\Provider\CoreServiceProvider;
@@ -12,6 +13,7 @@ $app = new App(require ROOT_DIR.'/app/config/settings.php');
 
 /** @var Container $container */
 $container = $app->getContainer();
+$container->register(new PimpleDumper());
 $container->register(new CoreServiceProvider($app));
 
 $app->run();
